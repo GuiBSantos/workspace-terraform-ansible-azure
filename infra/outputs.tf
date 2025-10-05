88f364ec85aea0494a3a43db13fdb1186b39991e
Guilherme Santos <guilhermesantos2004b@gmail.com>
2025-10-05 20:43:31 -0300

    Automatized Deploy

---
+++ infra/outputs.tf
@@ -0,0 +1,16 @@
+output "public_ip_address" {
+  value       = azurerm_public_ip.vm_public_ip.ip_address
+  description = "Endereço IP público da VM"
+}
+
+output "nsg_name" {
+  value       = azurerm_network_security_group.nsg.name
+  description = "Nome do Grupo de Segurança de Rede (NSG)"
+}
+
+output "resource_group_name" {
+  value       = azurerm_resource_group.rg.name
+  description = "Nome do Grupo de Recursos"
+}
+
+
---
+++ infra/variables.tf
@@ -0,0 +1,18 @@
+variable "resource_group_name" {
+  description = "Nome do Resource Group"
+    type        = string
+    default     = "rg-vn-automation"
+}
+
+variable "location" {
+  description = "Regiao da Azure"
+  type = string
+  default = "East US"
+}
+
+variable "admin_password" {
+  description = "Senha do usuario admin da VM"
+  type        = string
+  sensitive   = true
+  default = "SenhaForte123!"
+}
